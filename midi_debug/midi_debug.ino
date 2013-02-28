5cfa247fdc64fef0f03772f550d4d20181bbce41
Joseph B. Ottinger <joeo@enigmastation.com>
2013-02-28 14:09:11 -0500

    adding lcd display, octave display

--- midi_debug/midi_debug.ino
+++ midi_debug/midi_debug.ino
@@ -1,4 +1,7 @@
-#define FASTADC 1
+#include <MIDI.h>
+#include <LiquidCrystal.h>
+
+#define FASTADC 0
 
 // defines for setting and clearing register bits
 #ifndef cbi
@@ -9,6 +12,8 @@
 #endif
 
 const int STAT7=7;
+LiquidCrystal lcd(4,5, 8,9,10,11);
+int octave=2;
 
 void noteOff(int note) {
   Serial.print("-------------------------------------note off: ");
@@ -21,7 +26,7 @@ void noteOn(int note) {
 }
 
 const int SAMPLE_COUNT = 100;
-const int ACCEPTABLE_STDDEV=8;
+const int ACCEPTABLE_STDDEV=2;
 
 int ladder[]={ 
   285, 265, 250,
@@ -35,6 +40,7 @@ int ladder[]={
 int dataPoints[SAMPLE_COUNT];
 
 int readPin(int pin) {
+  int idx;
   long start=millis();
   double stddev;
   int loops=0;
@@ -50,19 +56,20 @@ int readPin(int pin) {
       stddev_sum+=(dataPoints[counter]-mean)*(dataPoints[counter]-mean);
     }
     stddev=sqrt(stddev_sum/SAMPLE_COUNT);
-long endTime=millis();
-Serial.print("elapsed time: ");
-Serial.println(endTime-start);
+    long endTime=millis();
+    Serial.print("elapsed time: ");
+    Serial.println(endTime-start);
     Serial.print("loops: ");
     Serial.println(loops);
     Serial.print("Average: ");
     Serial.println(mean);
     Serial.print("Standard Deviation: ");
     Serial.println(stddev);
+    idx=findIndex(mean);
   } 
-  while(mean<900 && stddev>ACCEPTABLE_STDDEV && (loops++)<5);
+  while(mean<900 && stddev>(ACCEPTABLE_STDDEV+(13-idx)) && (loops++)<5);
 
-  return mean;
+  return idx;
 }
 
 int findIndex(int frequency) {
@@ -93,18 +100,23 @@ void setup() {
   Serial.println("Hello!");
   pinMode(STAT7, OUTPUT);
   digitalWrite(STAT7, HIGH);
+  lcd.begin(16,2);
+  lcd.print("FrankenPedals v1");
+  lcd.setCursor(0,1);
+  lcd.print("O: ");
+  lcd.setCursor(3,1);
+  lcd.print(octave);
 }
 
 int counter=0;
 
 void loop() {
   int baseC1=24;
-  int octave=2;
   int lastIndex=-1;
   int index=lastIndex;
 
   while(true) {
-    index=findIndex(readPin(A2));
+    index=readPin(A2);
     if(index!=lastIndex) {
       if(lastIndex!=-1) {
         noteOff(baseC1+octave*12+lastIndex);
@@ -114,6 +126,10 @@ void loop() {
       }
     }
     lastIndex=index;
-    delay(500);
+    delay(150);
   }
 }
+
+
+
+
